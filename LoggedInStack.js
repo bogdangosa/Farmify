bda6b4da53308f83ed3d0e9b29652d94279f3176
bogdangosa <bogdan.gosa@gmail.com>
2024-04-05 21:57:23 +0300

    farmer screen for razvan

--- LoggedInStack.js
+++ LoggedInStack.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import AccountScreen from "./app/screens/AccountScreen";
 import HomeScreen from "./app/screens/HomeScreen";
+import FarmerScreen from "./app/screens/FarmerScreen";
 
 function LoggedInStack() {
   const Drawer = createDrawerNavigator();
@@ -10,6 +11,7 @@ function LoggedInStack() {
     <Drawer.Navigator initialRouteName="Home">
       <Drawer.Screen name="Home" component={HomeScreen}></Drawer.Screen>
       <Drawer.Screen name="Account" component={AccountScreen}></Drawer.Screen>
+      <Drawer.Screen name="Farmer" component={FarmerScreen}></Drawer.Screen>
     </Drawer.Navigator>
   );
 }
---
+++ app/screens/FarmerScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const FarmerScreen = () => {
+    return (
+        <View>
+            <Text>Farmer Screen</Text>
+        </View>
+    );
+};
+
+export default FarmerScreen;
