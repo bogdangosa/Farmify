76ce97c0a45f2046e4893174492e53e29674ef00
bogdangosa <bogdan.gosa@gmail.com>
2024-04-05 21:40:29 +0300

    sign out and sign up working

--- app/screens/AccountScreen.jsx
+++ app/screens/AccountScreen.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import SimpleButton from '../components/Buttons/SimpleButton';
+import { FIREBASE_AUTH } from '../../firebaseConfig';
 
 const AccountScreen = () => {
+
+    const SignOut = ()=>{
+        console.log("Signing out");
+        FIREBASE_AUTH.signOut();
+      }
+
     return (
         <View>
             <Text>Account Screen</Text>
+            <SimpleButton onPress={()=>SignOut()} title="Sign out"></SimpleButton>
         </View>
     );
 };
